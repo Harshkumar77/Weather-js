9eddbe84228df922bf89430f6ffd8288538a37d7
mighty_volcano <harshkumar@noreply.com>
2021-10-10 17:21:12 +0530

    finally ready

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const searchBar = document.querySelector(".search-bar");
 const stateName = document.querySelector(".State");
+const dateElement = document.querySelector(".date");
+const cond = document.querySelector(".cond");
+const temp = document.querySelector(".temp");
+const iconw = document.querySelector(".iconw");
+const sunset = document.querySelector(".sunset");
+const sunrise = document.querySelector(".sunrise");
+
+fetch("https://ipinfo.io/json")
+  .then((r) => r.json())
+  .then((data) => fetchAndUpdate(data.city));
 
 const API_KEY = "1561b2c4b7ada334819f833f3abb18eb";
 
@@ -25,7 +35,27 @@ async function fetchAndUpdate(state) {
     })
     .then((data) => {
       console.log(data);
-      console.log(data.weather[0].description);
+      console.log(data.main.feels_like - 273);
+      temp.textContent = Math.floor(data.main.temp - 273) + " °C";
+      cond.textContent = data.weather[0].description;
       stateName.textContent = data.name;
+      var tt = new Date();
+      // console.log(tt.slice(0, 21));
+      // dateElement.textContent = tt.slice(0, 21);
+      dateElement.textContent = "As of " + tt.toLocaleTimeString().slice(0, 5);
+      updateIconAndBkg(data.weather[0].id, data.weather[0].icon);
+      sunrise.textContent = new Date(1000 * data.sys.sunrise)
+        .toLocaleTimeString()
+        .slice(0, 5);
+      sunset.textContent = new Date(1000 * data.sys.sunset)
+        .toLocaleTimeString()
+        .slice(0, 5);
     });
+}
+
+function updateIconAndBkg(id, iconid) {
+  console.log(id, iconid);
+  if (iconid === "50d" || iconid === "50n")
+    iconw.setAttribute("src", "images/weather-icons/" + id + ".png");
+  else iconw.setAttribute("src", "images/weather-icons/" + iconid + ".png");
 }
